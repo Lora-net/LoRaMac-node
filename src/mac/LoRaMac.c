b633c7b04bb225a0c7eb924c77cd413ac2f71666
Daniel Jäckle <daniel.jaeckle@stackforce.de>
2016-07-29 11:36:24 +0200

    Bug fix in definition of datarateOffsets for US band.

--- src/mac/LoRaMac.c
+++ src/mac/LoRaMac.c
@@ -327,7 +327,7 @@ const uint8_t Datarates[]  = { 10, 9, 8,  7,  8,  0,  0, 0, 12, 11, 10, 9, 8, 7,
 /*!
  * Up/Down link data rates offset definition
  */
-const int8_t datarateOffsets[16][4] =
+const int8_t datarateOffsets[5][4] =
 {
     { DR_10, DR_9 , DR_8 , DR_8  }, // DR_0
     { DR_11, DR_10, DR_9 , DR_8  }, // DR_1
