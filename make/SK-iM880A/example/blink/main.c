a77e83237619625dfe0d53431129009450eedd27
Samuel Rohrer <srohrer32@gmail.com>
2016-09-14 16:49:07 -0400

    blink app working on SK-iM880B to flash LEDs

--- make/SK-iM880A/example/blink/main.c
+++ make/SK-iM880A/example/blink/main.c
@@ -1,3 +1,8 @@
+#include "gpio.h"
+#include "gpio-board.h"
+#include "pinName-board.h"
+#include "stm32l1xx.h"
+#include "stm32l1xx_hal.h"
 #include "stm32l1xx_hal_gpio.h"
 #include "stm32l1xx_hal_rcc_ex.h"
 
@@ -16,40 +21,72 @@ void delay(int count) {
 int main(void) {
 
   /* GPIOC Periph clock enable */
-  // RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+  /* RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
   RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
   PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
   PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
+  PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
   HAL_RCCEx_GetPeriphCLKConfig(&PeriphClkInitStruct);
+  HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
 
-  GPIO_InitStructure.Pin = GPIO_PIN_8 | GPIO_PIN_9;
+  GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_0;
   GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
-  // GPIO_InitStructure.OType = GPIO_OType_PP;
+  GPIO_InitStructure.OType = GPIO_OType_PP;
   GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
   GPIO_InitStructure.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  GPIOA->MODER = 0x55555555;   // set pin 0 and 1 as output
+  GPIOA->OTYPER = 0x0;         // set both pins as output push/pull
+  GPIOA->OSPEEDR = 0xFFFFFFFF; // set both pins as very high speed
+  GPIOA->PUPDR = 0x55555555;   // set both pins to pull up
+*/
+
+  // using same method as system_stm32l1xx.c
+  __IO uint32_t tmpreg = 0;
+  FLASH->ACR |= FLASH_ACR_LATENCY;
+  RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+  tmpreg = READ_BIT(RCC->APB1ENR, RCC_APB1ENR_PWREN);
+  PWR->CR = PWR_CR_VOS_0;
+
+  // wait for voltage regulator to be ready
+  while ((PWR->CSR & PWR_CSR_VOSF) != RESET) {
+  }
+  RCC->AHBENR = 0x000080DF;
+  tmpreg = READ_BIT(RCC->AHBENR, RCC_AHBENR_GPIODEN);
+  GPIOA->MODER = 0xa8555555;
+  GPIOA->OTYPER = 0x0;
+  GPIOA->OSPEEDR = 0xFFFFFFFF;
+  GPIOA->PUPDR = 0x64555555;
+
+  (void)(tmpreg);
 
   for (;;) {
     // Turn on both LEDs
-    GPIOC->BSRR = 0x0300;
+    /*GPIOA->BSRR = 0x03;
 
     // Around 1/4 of a second
     delay(2400000);
 
     // Turn off the first led
-//    GPIOC->BGRR = 0x0100;
+    GPIOA->BRR= 0x01;
 
     delay(2400000);
 
     // Turn off the second LED and the on the first
- //   GPIOC->BGRR = 0x0200;
-    GPIOC->BSRR = 0x0100;
+    GPIOA->BRR = 0x02;
+    GPIOA->BSRR = 0x01;
 
     delay(2400000);
 
     // Turn off first
-  //  GPIOC->BGRR = 0x0100;
+    GPIOA->BRR = 0x01;
 
-    delay(2400000);
+    delay(2400000); */
+
+    GPIOA->BSRR = 0xFFFF;
+    delay(24000);
+    GPIOA->BSRR = 0xFFFF0000;
+    delay(24000);
   }
 }
