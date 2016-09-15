2ec84fd7fa5ba01fafacb11359c76fb7bd1d7480
Samuel Rohrer <srohrer32@gmail.com>
2016-09-15 17:43:55 -0400

    blink app working with HAL

--- make/SK-iM880A/example/blink/main.c
+++ make/SK-iM880A/example/blink/main.c
@@ -20,30 +20,21 @@ void delay(int count) {
 /// Main function.  Called by the startup code.
 int main(void) {
 
-  /* GPIOC Periph clock enable */
-  /* RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
   RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
   PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
   PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
-  PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
-  HAL_RCCEx_GetPeriphCLKConfig(&PeriphClkInitStruct);
   HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
+  //RCC->AHBENR = 0x00008001;
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_0;
+  GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_0 | GPIO_PIN_8;
   GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStructure.OType = GPIO_OType_PP;
   GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
   GPIO_InitStructure.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-  GPIOA->MODER = 0x55555555;   // set pin 0 and 1 as output
-  GPIOA->OTYPER = 0x0;         // set both pins as output push/pull
-  GPIOA->OSPEEDR = 0xFFFFFFFF; // set both pins as very high speed
-  GPIOA->PUPDR = 0x55555555;   // set both pins to pull up
-*/
-
   // using same method as system_stm32l1xx.c
-  __IO uint32_t tmpreg = 0;
+  /* __IO uint32_t tmpreg = 0;
   FLASH->ACR |= FLASH_ACR_LATENCY;
   RCC->APB1ENR |= RCC_APB1ENR_PWREN;
   tmpreg = READ_BIT(RCC->APB1ENR, RCC_APB1ENR_PWREN);
@@ -52,14 +43,11 @@ int main(void) {
   // wait for voltage regulator to be ready
   while ((PWR->CSR & PWR_CSR_VOSF) != RESET) {
   }
-  RCC->AHBENR = 0x000080DF;
-  tmpreg = READ_BIT(RCC->AHBENR, RCC_AHBENR_GPIODEN);
   GPIOA->MODER = 0xa8555555;
   GPIOA->OTYPER = 0x0;
   GPIOA->OSPEEDR = 0xFFFFFFFF;
   GPIOA->PUPDR = 0x64555555;
-
-  (void)(tmpreg);
+*/
 
   for (;;) {
     // Turn on both LEDs
