1c78f4df8b9f3dd360e27012aaf21c674c4246f5
Miguel Luis <mluis@semtech.com>
2016-10-05 13:50:39 +0200

    Added a check that verifies if DIO4 is connected on DIO2 interrupt handler.

--- src/radio/sx1272/sx1272.c
+++ src/radio/sx1272/sx1272.c
@@ -1497,6 +1497,12 @@ void SX1272OnDio2Irq( void )
             switch( SX1272.Settings.Modem )
             {
             case MODEM_FSK:
+                // Checks if DIO4 is connected. If it is not PreambleDtected is set to true.
+                if( SX1272.DIO4.port == NULL )
+                {
+                    SX1272.Settings.FskPacketHandler.PreambleDetected = true;
+                }
+
                 if( ( SX1272.Settings.FskPacketHandler.PreambleDetected == true ) && ( SX1272.Settings.FskPacketHandler.SyncWordDetected == false ) )
                 {
                     TimerStop( &RxTimeoutSyncWord );
