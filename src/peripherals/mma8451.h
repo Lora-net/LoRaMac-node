4af921e4018bf22683dbaaedcc0efd9e7a9ec799
Miguel Luis <mluis@semtech.com>
2017-02-02 18:17:53 +0100

    Added orientation detection to the MMA8451Q peripheral driver.

--- src/peripherals/mma8451.h
+++ src/peripherals/mma8451.h
@@ -23,7 +23,27 @@ Maintainer: Miguel Luis and Gregory Cristian
 /*
  * MMA8451 Registers
  */ 
-#define MMA8451_ID                                   0x0D
+#define MMA8451_STATUS                               0x00 //
+#define MMA8451_OUT_X_MSB                            0x01 //
+#define MMA8451_SYSMOD                               0x0B //
+#define MMA8451_INT_SOURCE                           0x0C //
+#define MMA8451_ID                                   0x0D //
+#define MMA8451_PL_STATUS                            0x10 //
+#define MMA8451_PL_CFG                               0x11 //
+#define MMA8451_PL_COUNT                             0x12 // Orientation debounce
+#define MMA8451_PL_BF_ZCOMP                          0x13 //
+#define MMA8451_PL_THS_REG                           0x14 //
+#define MMA8451_FF_MT_SRC                            0x16 //
+#define MMA8451_TRANSIENT_CFG                        0x1D // Transient enable
+#define MMA8451_TRANSIENT_SRC                        0x1E // Transient read/clear interrupt
+#define MMA8451_TRANSIENT_THS                        0x1F // Transient threshold
+#define MMA8451_TRANSIENT_COUNT                      0x20 // Transient debounce
+#define MMA8451_PULSE_SRC                            0x22 //
+#define MMA8451_CTRL_REG1                            0x2A //
+#define MMA8451_CTRL_REG2                            0x2B //
+#define MMA8451_CTRL_REG3                            0x2C // Interrupt control
+#define MMA8451_CTRL_REG4                            0x2D // Interrupt enable
+#define MMA8451_CTRL_REG5                            0x2E // Interrupt pin selection
 
 /*!
  * \brief Initializes the device
@@ -40,55 +60,17 @@ uint8_t MMA8451Init( void );
 uint8_t MMA8451Reset( void );
 
 /*!
- * \brief Writes a byte at specified address in the device
- *
- * \param [IN]: addr
- * \param [IN]: data
- * \retval status [SUCCESS, FAIL]
+ * \brief Initializes the orientation detection
  */
-uint8_t MMA8451Write( uint8_t addr, uint8_t data );
+void MMA8451OrientDetect( void );
 
 /*!
- * \brief Writes a buffer at specified address in the device
+ * \brief Gets the orientation state.
  *
- * \param [IN]: addr
- * \param [IN]: data
- * \param [IN]: size
- * \retval status [SUCCESS, FAIL]
+ * \retval orientation Bit 6 [1: Horizontal, 0: Vertical]
+ *                     Bit 0 [1: Face down, 0: Face up]
+ *                     Other bits don't care.
  */
-uint8_t MMA8451WriteBuffer( uint8_t addr, uint8_t *data, uint8_t size );
-
-/*!
- * \brief Reads a byte at specified address in the device
- *
- * \param [IN]: addr
- * \param [OUT]: data
- * \retval status [SUCCESS, FAIL]
- */
-uint8_t MMA8451Read( uint8_t addr, uint8_t *data );
-
-/*!
- * \brief Reads a buffer at specified address in the device
- *
- * \param [IN]: addr
- * \param [OUT]: data
- * \param [IN]: size
- * \retval status [SUCCESS, FAIL]
- */
-uint8_t MMA8451ReadBuffer( uint8_t addr, uint8_t *data, uint8_t size );
-
-/*!
- * \brief Sets the I2C device slave address
- *
- * \param [IN]: addr
- */
-void MMA8451SetDeviceAddr( uint8_t addr );
-
-/*!
- * \brief Gets the I2C device slave address
- *
- * \retval: addr Current device slave address
- */
-uint8_t MMA8451GetDeviceAddr( void );
+uint8_t MMA8451GetOrientation( void );
 
 #endif  // __MMA8451_H__
