0d8a722a85b8ce8829d0aaa89c9ceba1e07621e6
Daniel JÃ¤ckle <daniel.jaeckle@stackforce.de>
2017-03-02 14:43:53 +0100

    Update file header comments.

--- src/mac/LoRaMac.h
+++ src/mac/LoRaMac.h
@@ -26,7 +26,7 @@
  *
  * \author    Gregory Cristian ( Semtech )
  *
- * \author    Daniel Jäckle ( STACKFORCE )
+ * \author    Daniel Jaeckle ( STACKFORCE )
  *
  * \defgroup  LORAMAC LoRa MAC layer implementation
  *            This module specifies the API implementation of the LoRaMAC layer.
