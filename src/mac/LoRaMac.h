d15d46e12fbeebc77684668165f35486943a3760
Miguel Luis <mluis@semtech.com>
2017-03-17 11:41:43 +0100

    Fix node stopping issue. When the node receives a frame with an invalid CRC in RX1, it could happen, that the node stops to transmit uplinks.

--- src/mac/LoRaMac.h
+++ src/mac/LoRaMac.h
@@ -566,6 +566,10 @@ typedef enum eLoRaMacEventInfoStatus
      */
     LORAMAC_EVENT_INFO_STATUS_RX2_TIMEOUT,
     /*!
+     * An Rx error occurred on receive window 1
+     */
+    LORAMAC_EVENT_INFO_STATUS_RX1_ERROR,
+    /*!
      * An Rx error occurred on receive window 2
      */
     LORAMAC_EVENT_INFO_STATUS_RX2_ERROR,
