51d1abd105cbaeaf5107e2a670db546add2f29cb
Nestor Ayuso <nestorayuso@gmail.com>
2017-10-09 15:02:46 +0200

    Update RegionCN779.h

--- src/mac/region/RegionCN779.h
+++ src/mac/region/RegionCN779.h
@@ -194,9 +194,9 @@
 
 /*!
  * Band 0 definition
- * { DutyCycle, TxMaxPower, LastTxDoneTime, TimeOff }
+ * { DutyCycle, TxMaxPower, LastJoinTxDoneTime, LastTxDoneTime, TimeOff }
  */
-#define CN779_BAND0                                 { 100, CN779_MAX_TX_POWER, 0,  0 } //  1.0 %
+#define CN779_BAND0                                 { 100, CN779_MAX_TX_POWER, 0, 0, 0 } //  1.0 %
 
 /*!
  * LoRaMac default channel 1
