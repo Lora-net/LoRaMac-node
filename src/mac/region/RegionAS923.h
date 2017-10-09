1288961f900cf4b26d669a5bca5f6a7ffc61ea5d
Nestor Ayuso <nestorayuso@gmail.com>
2017-10-09 14:58:43 +0200

    Update RegionAS923.h

--- src/mac/region/RegionAS923.h
+++ src/mac/region/RegionAS923.h
@@ -207,9 +207,9 @@
 
 /*!
  * Band 0 definition
- * { DutyCycle, TxMaxPower, LastTxDoneTime, TimeOff }
+ * { DutyCycle, TxMaxPower, LastJoinTxDoneTime, LastTxDoneTime, TimeOff }
  */
-#define AS923_BAND0                                 { 100, AS923_MAX_TX_POWER, 0,  0 } //  1.0 %
+#define AS923_BAND0                                 { 100, AS923_MAX_TX_POWER, 0, 0, 0 } //  1.0 %
 
 /*!
  * LoRaMac default channel 1
