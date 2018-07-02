6a7acc98fd43ad417b04ffbe9d07da5df302590b
Miguel Luis <mluis@semtech.com>
2018-07-02 14:08:29 +0200

    Update main examples for 1.1.0 support.

--- src/apps/LoRaMac/classB/NucleoL073/main.c
+++ src/apps/LoRaMac/classB/NucleoL073/main.c
@@ -406,7 +406,6 @@ static bool SendFrame( void )
             mcpsReq.Req.Confirmed.fPort = AppPort;
             mcpsReq.Req.Confirmed.fBuffer = AppDataBuffer;
             mcpsReq.Req.Confirmed.fBufferSize = AppDataSize;
-            mcpsReq.Req.Confirmed.NbTrials = 8;
             mcpsReq.Req.Confirmed.Datarate = LORAWAN_DEFAULT_DATARATE;
         }
     }
@@ -516,7 +515,7 @@ static void McpsConfirm( McpsConfirm_t *mcpsConfirm )
                 // Check Datarate
                 // Check TxPower
                 // Check AckReceived
-                // Check NbTrials
+                // Check NbTrans
                 break;
             }
             case MCPS_PROPRIETARY:
