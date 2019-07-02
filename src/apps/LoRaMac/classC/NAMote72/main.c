0bd3b6a498b362be95b6c0d7b925e92776df790e
Miguel Luis <mluis@semtech.com>
2019-07-02 14:45:42 +0200

    Update ClassC examples due to changes done on commit 198a8295be4a51f16384c8c107b4abdcf52683fd

--- src/apps/LoRaMac/classC/NAMote72/main.c
+++ src/apps/LoRaMac/classC/NAMote72/main.c
@@ -1166,10 +1166,6 @@ int main( void )
                 mibReq.Param.SystemMaxRxError = 20;
                 LoRaMacMibSetRequestConfirm( &mibReq );
 
-                mibReq.Type = MIB_DEVICE_CLASS;
-                mibReq.Param.Class = CLASS_C;
-                LoRaMacMibSetRequestConfirm( &mibReq );
-
                 LoRaMacStart( );
 
                 mibReq.Type = MIB_NETWORK_ACTIVATION;
@@ -1240,6 +1236,15 @@ int main( void )
             {
                 if( NextTx == true )
                 {
+                    mibReq.Type = MIB_DEVICE_CLASS;
+                    LoRaMacMibGetRequestConfirm( &mibReq );
+
+                    if( mibReq.Param.Class!= CLASS_C )
+                    {
+                        mibReq.Param.Class = CLASS_C;
+                        LoRaMacMibSetRequestConfirm( &mibReq );
+                    }
+
                     PrepareTxFrame( AppPort );
 
                     NextTx = SendFrame( );
