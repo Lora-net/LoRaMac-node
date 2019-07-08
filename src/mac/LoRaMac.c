c9018a74aeef3cb21d2922151e1d2c80d085aaa3
taigaiot <konstantin@taigaiot.com>
2019-07-08 16:29:02 +0700

    Revert "RX1 error handling added in HandleRadioRxErrorTimeout function"
    This reverts commit 06cac5b679e7b352ae15ea404e650d328b65c1c0.

--- src/mac/LoRaMac.c
+++ src/mac/LoRaMac.c
@@ -1374,11 +1374,7 @@ static void HandleRadioRxErrorTimeout( LoRaMacEventInfoStatus_t rx1EventInfoStat
             classBRx = true;
         }
     }
-    if(rx1EventInfoStatus == LORAMAC_EVENT_INFO_STATUS_RX1_ERROR)
-    {
-      MacCtx.MacFlags.Bits.MacDone = 1;
-    } 
-    
+
     if( classBRx == false )
     {
         if( MacCtx.RxSlot == RX_SLOT_WIN_1 )
