ab6d2096983cb87583c8f72bf85754146cd93e4c
Manivannan Sadhasivam <mani@kernel.org>
2020-03-04 17:18:15 +0530

    Move MacStatusStrings and EventInfoStatusStrings to a common place
    Since these arrays are used throughout all platforms, it makes sense
    to move it to a common place (LoRaMac.h) to avoid code duplication.
    
    Still there is one more place where these arrays are defined,
    "LoRaMac/common/LmHandlerMsgDisplay.c" but I've left it since LoRaMac.h
    is not included in that file.
    
    While moving these, I can also reuse these arrays in Zephyr LoRaWAN port.
    
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>

--- src/apps/LoRaMac/classB/NAMote72/main.c
+++ src/apps/LoRaMac/classB/NAMote72/main.c
@@ -261,61 +261,6 @@ extern Gpio_t Led2; // Rx and blinks every 5 seconds when beacon is acquired
 extern Gpio_t Led3; // App
 
 /*!
- * MAC status strings
- */
-const char* MacStatusStrings[] =
-{
-    "OK",                            // LORAMAC_STATUS_OK
-    "Busy",                          // LORAMAC_STATUS_BUSY
-    "Service unknown",               // LORAMAC_STATUS_SERVICE_UNKNOWN
-    "Parameter invalid",             // LORAMAC_STATUS_PARAMETER_INVALID
-    "Frequency invalid",             // LORAMAC_STATUS_FREQUENCY_INVALID
-    "Datarate invalid",              // LORAMAC_STATUS_DATARATE_INVALID
-    "Frequency or datarate invalid", // LORAMAC_STATUS_FREQ_AND_DR_INVALID
-    "No network joined",             // LORAMAC_STATUS_NO_NETWORK_JOINED
-    "Length error",                  // LORAMAC_STATUS_LENGTH_ERROR
-    "Region not supported",          // LORAMAC_STATUS_REGION_NOT_SUPPORTED
-    "Skipped APP data",              // LORAMAC_STATUS_SKIPPED_APP_DATA
-    "Duty-cycle restricted",         // LORAMAC_STATUS_DUTYCYCLE_RESTRICTED
-    "No channel found",              // LORAMAC_STATUS_NO_CHANNEL_FOUND
-    "No free channel found",         // LORAMAC_STATUS_NO_FREE_CHANNEL_FOUND
-    "Busy beacon reserved time",     // LORAMAC_STATUS_BUSY_BEACON_RESERVED_TIME
-    "Busy ping-slot window time",    // LORAMAC_STATUS_BUSY_PING_SLOT_WINDOW_TIME
-    "Busy uplink collision",         // LORAMAC_STATUS_BUSY_UPLINK_COLLISION
-    "Crypto error",                  // LORAMAC_STATUS_CRYPTO_ERROR
-    "FCnt handler error",            // LORAMAC_STATUS_FCNT_HANDLER_ERROR
-    "MAC command error",             // LORAMAC_STATUS_MAC_COMMAD_ERROR
-    "ClassB error",                  // LORAMAC_STATUS_CLASS_B_ERROR
-    "Confirm queue error",           // LORAMAC_STATUS_CONFIRM_QUEUE_ERROR
-    "Multicast group undefined",     // LORAMAC_STATUS_MC_GROUP_UNDEFINED
-    "Unknown error",                 // LORAMAC_STATUS_ERROR
-};
-
-/*!
- * MAC event info status strings.
- */
-const char* EventInfoStatusStrings[] =
-{ 
-    "OK",                            // LORAMAC_EVENT_INFO_STATUS_OK
-    "Error",                         // LORAMAC_EVENT_INFO_STATUS_ERROR
-    "Tx timeout",                    // LORAMAC_EVENT_INFO_STATUS_TX_TIMEOUT
-    "Rx 1 timeout",                  // LORAMAC_EVENT_INFO_STATUS_RX1_TIMEOUT
-    "Rx 2 timeout",                  // LORAMAC_EVENT_INFO_STATUS_RX2_TIMEOUT
-    "Rx1 error",                     // LORAMAC_EVENT_INFO_STATUS_RX1_ERROR
-    "Rx2 error",                     // LORAMAC_EVENT_INFO_STATUS_RX2_ERROR
-    "Join failed",                   // LORAMAC_EVENT_INFO_STATUS_JOIN_FAIL
-    "Downlink repeated",             // LORAMAC_EVENT_INFO_STATUS_DOWNLINK_REPEATED
-    "Tx DR payload size error",      // LORAMAC_EVENT_INFO_STATUS_TX_DR_PAYLOAD_SIZE_ERROR
-    "Downlink too many frames loss", // LORAMAC_EVENT_INFO_STATUS_DOWNLINK_TOO_MANY_FRAMES_LOSS
-    "Address fail",                  // LORAMAC_EVENT_INFO_STATUS_ADDRESS_FAIL
-    "MIC fail",                      // LORAMAC_EVENT_INFO_STATUS_MIC_FAIL
-    "Multicast fail",                // LORAMAC_EVENT_INFO_STATUS_MULTICAST_FAIL
-    "Beacon locked",                 // LORAMAC_EVENT_INFO_STATUS_BEACON_LOCKED
-    "Beacon lost",                   // LORAMAC_EVENT_INFO_STATUS_BEACON_LOST
-    "Beacon not found"               // LORAMAC_EVENT_INFO_STATUS_BEACON_NOT_FOUND
-};
-
-/*!
  * Prints the provided buffer in HEX
  * 
  * \param buffer Buffer to be printed
