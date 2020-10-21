c6ef8a864b47aeb715031c1e91a95525aa6c0d1c
MedadRufus <mailto@medadnewman.co.uk>
2020-10-21 14:38:18 +0100

    Add in Keil support and adapted to hardware on the picotracker.
    Serial port is changed to UART1.
    
    put in keil periodic uplink project
    
    switch to using USART1 pins, used on picotracker
    
    ICSPACE23 ABP credentials added
    
    Use ADR and default rate of DR_4
    
    put in icspace24 credentials
    
    update device pack from st
    
    setup settings file for vs code. works.
    
    can compile now with vs code and arm embedded tool chain.
    
    update some features in the uvprojx file
    
    change serial baudrate to 115200 baud
    
    change back uart speed to 2 Mbaud
    
    Or else, it seems to get stuck.
    
    Update periodic-uplink-lpp.uvprojx
    
    put in correct keil config

--- src/boards/B-L072Z-LRWAN1/uart-board.c
+++ src/boards/B-L072Z-LRWAN1/uart-board.c
@@ -36,7 +36,7 @@ static UART_HandleTypeDef UartHandle;
 uint8_t RxData = 0;
 uint8_t TxData = 0;
 
-extern Uart_t Uart2;
+extern Uart_t Uart1;
 
 void UartMcuInit( Uart_t *obj, UartId_t uartId, PinNames tx, PinNames rx )
 {
@@ -50,12 +50,12 @@ void UartMcuInit( Uart_t *obj, UartId_t uartId, PinNames tx, PinNames rx )
     }
     else
     {
-        __HAL_RCC_USART2_FORCE_RESET( );
-        __HAL_RCC_USART2_RELEASE_RESET( );
-        __HAL_RCC_USART2_CLK_ENABLE( );
+        __HAL_RCC_USART1_FORCE_RESET( );
+        __HAL_RCC_USART1_RELEASE_RESET( );
+        __HAL_RCC_USART1_CLK_ENABLE( );
 
-        GpioInit( &obj->Tx, tx, PIN_ALTERNATE_FCT, PIN_PUSH_PULL, PIN_PULL_UP, GPIO_AF4_USART2 );
-        GpioInit( &obj->Rx, rx, PIN_ALTERNATE_FCT, PIN_PUSH_PULL, PIN_PULL_UP, GPIO_AF4_USART2 );
+        GpioInit( &obj->Tx, tx, PIN_ALTERNATE_FCT, PIN_PUSH_PULL, PIN_PULL_UP, GPIO_AF4_USART1 );
+        GpioInit( &obj->Rx, rx, PIN_ALTERNATE_FCT, PIN_PUSH_PULL, PIN_PULL_UP, GPIO_AF4_USART1 );
     }
 }
 
@@ -69,7 +69,7 @@ void UartMcuConfig( Uart_t *obj, UartMode_t mode, uint32_t baudrate, WordLength_
     }
     else
     {
-        UartHandle.Instance = USART2;
+        UartHandle.Instance = USART1;
         UartHandle.Init.BaudRate = baudrate;
 
         if( mode == TX_ONLY )
@@ -161,8 +161,8 @@ void UartMcuConfig( Uart_t *obj, UartMode_t mode, uint32_t baudrate, WordLength_
             assert_param( FAIL );
         }
 
-        HAL_NVIC_SetPriority( USART2_IRQn, 1, 0 );
-        HAL_NVIC_EnableIRQ( USART2_IRQn );
+        HAL_NVIC_SetPriority( USART1_IRQn, 1, 0 );
+        HAL_NVIC_EnableIRQ( USART1_IRQn );
 
         /* Enable the UART Data Register not empty Interrupt */
         HAL_UART_Receive_IT( &UartHandle, &RxData, 1 );
@@ -179,9 +179,9 @@ void UartMcuDeInit( Uart_t *obj )
     }
     else
     {
-        __HAL_RCC_USART2_FORCE_RESET( );
-        __HAL_RCC_USART2_RELEASE_RESET( );
-        __HAL_RCC_USART2_CLK_DISABLE( );
+        __HAL_RCC_USART1_FORCE_RESET( );
+        __HAL_RCC_USART1_RELEASE_RESET( );
+        __HAL_RCC_USART1_CLK_DISABLE( );
 
         GpioInit( &obj->Tx, obj->Tx.pin, PIN_ANALOGIC, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
         GpioInit( &obj->Rx, obj->Rx.pin, PIN_ANALOGIC, PIN_PUSH_PULL, PIN_NO_PULL, 0 );
@@ -303,30 +303,30 @@ uint8_t UartMcuGetBuffer( Uart_t *obj, uint8_t *buffer, uint16_t size, uint16_t 
 
 void HAL_UART_TxCpltCallback( UART_HandleTypeDef *handle )
 {
-    if( IsFifoEmpty( &Uart2.FifoTx ) == false )
+    if( IsFifoEmpty( &Uart1.FifoTx ) == false )
     {
-        TxData = FifoPop( &Uart2.FifoTx );
+        TxData = FifoPop( &Uart1.FifoTx );
         //  Write one byte to the transmit data register
         HAL_UART_Transmit_IT( &UartHandle, &TxData, 1 );
     }
 
-    if( Uart2.IrqNotify != NULL )
+    if( Uart1.IrqNotify != NULL )
     {
-        Uart2.IrqNotify( UART_NOTIFY_TX );
+        Uart1.IrqNotify( UART_NOTIFY_TX );
     }
 }
 
 void HAL_UART_RxCpltCallback( UART_HandleTypeDef *handle )
 {
-    if( IsFifoFull( &Uart2.FifoRx ) == false )
+    if( IsFifoFull( &Uart1.FifoRx ) == false )
     {
         // Read one byte from the receive data register
-        FifoPush( &Uart2.FifoRx, RxData );
+        FifoPush( &Uart1.FifoRx, RxData );
     }
 
-    if( Uart2.IrqNotify != NULL )
+    if( Uart1.IrqNotify != NULL )
     {
-        Uart2.IrqNotify( UART_NOTIFY_RX );
+        Uart1.IrqNotify( UART_NOTIFY_RX );
     }
 
     HAL_UART_Receive_IT( &UartHandle, &RxData, 1 );
@@ -337,7 +337,7 @@ void HAL_UART_ErrorCallback( UART_HandleTypeDef *handle )
     HAL_UART_Receive_IT( &UartHandle, &RxData, 1 );
 }
 
-void USART2_IRQHandler( void )
+void USART1_IRQHandler( void )
 {
     HAL_UART_IRQHandler( &UartHandle );
 }
--- src/peripherals/soft-se/se-identity.h
+++ src/peripherals/soft-se/se-identity.h
@@ -66,17 +66,17 @@ extern "C" {
  * When set to 1 DevEui is LORAWAN_DEVICE_EUI
  * When set to 0 DevEui is automatically set with a value provided by MCU platform
  */
-#define STATIC_DEVICE_EUI                                  0
+#define STATIC_DEVICE_EUI                                  1
 
 /*!
  * end-device IEEE EUI (big endian)
  */
-#define LORAWAN_DEVICE_EUI                                 { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+#define LORAWAN_DEVICE_EUI                                 { 0x00, 0xED, 0xDF, 0x3B, 0x7A, 0xDA, 0x7F, 0x6B }
 
 /*!
  * App/Join server IEEE EUI (big endian)
  */
-#define LORAWAN_JOIN_EUI                                   { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+#define LORAWAN_JOIN_EUI                                   { 0x70, 0xB3, 0xD5, 0x7E, 0xD0, 0x02, 0x82, 0x4D }
 
 /*!
  * Secure-element pin
@@ -88,12 +88,12 @@ extern "C" {
  * When set to 0 DevAddr is automatically set with a value provided by a pseudo
  *      random generator seeded with a value provided by the MCU platform
  */
-#define STATIC_DEVICE_ADDRESS                              0
+#define STATIC_DEVICE_ADDRESS                              1
 
 /*!
  * Device address on the network (big endian)
  */
-#define LORAWAN_DEVICE_ADDRESS                             ( uint32_t )0x00000000
+#define LORAWAN_DEVICE_ADDRESS                             ( uint32_t )0x26013D75
 
 #define SOFT_SE_KEY_LIST                                                                                            \
     {                                                                                                               \
@@ -103,8 +103,7 @@ extern "C" {
              * WARNING: FOR 1.0.x DEVICES IT IS THE \ref LORAWAN_GEN_APP_KEY                                        \
              */                                                                                                     \
             .KeyID    = APP_KEY,                                                                                    \
-            .KeyValue = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, \
-                          0x3C },                                                                                   \
+            .KeyValue = { 0xB5, 0x21, 0xC6, 0x23, 0xA8, 0x94, 0x80, 0x90, 0xAD, 0xC7, 0x19, 0x6F, 0x4B, 0x31, 0x4B, 0x3A }, \
         },                                                                                                          \
         {                                                                                                           \
             /*!                                                                                                     \
@@ -112,8 +111,7 @@ extern "C" {
              * WARNING: FOR 1.0.x DEVICES IT IS THE \ref LORAWAN_APP_KEY                                            \
              */                                                                                                     \
             .KeyID    = NWK_KEY,                                                                                    \
-            .KeyValue = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, \
-                          0x3C },                                                                                   \
+            .KeyValue = { 0xB5, 0x21, 0xC6, 0x23, 0xA8, 0x94, 0x80, 0x90, 0xAD, 0xC7, 0x19, 0x6F, 0x4B, 0x31, 0x4B, 0x3A }, \
         },                                                                                                          \
         {                                                                                                           \
             /*!                                                                                                     \
@@ -139,8 +137,7 @@ extern "C" {
              * WARNING: NWK_S_KEY FOR 1.0.x DEVICES                                                                 \
              */                                                                                                     \
             .KeyID    = F_NWK_S_INT_KEY,                                                                            \
-            .KeyValue = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, \
-                          0x3C },                                                                                   \
+            .KeyValue = { 0xB5, 0x21, 0xC6, 0x23, 0xA8, 0x94, 0x80, 0x90, 0xAD, 0xC7, 0x19, 0x6F, 0x4B, 0x31, 0x4B, 0x3A }, \
         },                                                                                                          \
         {                                                                                                           \
             /*!                                                                                                     \
@@ -148,8 +145,7 @@ extern "C" {
              * WARNING: NOT USED FOR 1.0.x DEVICES. MUST BE THE SAME AS \ref LORAWAN_F_NWK_S_INT_KEY                \
              */                                                                                                     \
             .KeyID    = S_NWK_S_INT_KEY,                                                                            \
-            .KeyValue = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, \
-                          0x3C },                                                                                   \
+            .KeyValue = { 0xB5, 0x21, 0xC6, 0x23, 0xA8, 0x94, 0x80, 0x90, 0xAD, 0xC7, 0x19, 0x6F, 0x4B, 0x31, 0x4B, 0x3A }, \
         },                                                                                                          \
         {                                                                                                           \
             /*!                                                                                                     \
@@ -157,16 +153,14 @@ extern "C" {
              * WARNING: NOT USED FOR 1.0.x DEVICES. MUST BE THE SAME AS \ref LORAWAN_F_NWK_S_INT_KEY                \
              */                                                                                                     \
             .KeyID    = NWK_S_ENC_KEY,                                                                              \
-            .KeyValue = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, \
-                          0x3C },                                                                                   \
+            .KeyValue = { 0xB5, 0x21, 0xC6, 0x23, 0xA8, 0x94, 0x80, 0x90, 0xAD, 0xC7, 0x19, 0x6F, 0x4B, 0x31, 0x4B, 0x3A }, \
         },                                                                                                          \
         {                                                                                                           \
             /*!                                                                                                     \
              * Application session key                                                                              \
              */                                                                                                     \
             .KeyID    = APP_S_KEY,                                                                                  \
-            .KeyValue = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, \
-                          0x3C },                                                                                   \
+            .KeyValue = { 0xCE, 0x27, 0xD7, 0xC6, 0x98, 0xE1, 0x8C, 0xE2, 0x4F, 0xAA, 0xB4, 0x62, 0xFF, 0xD4, 0xAF, 0x94 },\
         },                                                                                                          \
         {                                                                                                           \
             /*!                                                                                                     \
