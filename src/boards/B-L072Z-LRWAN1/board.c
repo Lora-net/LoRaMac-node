8d78447b3f2e3fcb2094df53af83181dbb3eddbc
MedadRufus <mailto@medadnewman.co.uk>
2020-10-21 16:23:06 +0100

    Properly intialise the LSI clock

--- src/boards/B-L072Z-LRWAN1/board.c
+++ src/boards/B-L072Z-LRWAN1/board.c
@@ -250,7 +250,7 @@ void SystemClockConfig( void )
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_LSI;
     RCC_OscInitStruct.HSEState = RCC_HSE_OFF;
     RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.LSEState = RCC_LSI_ON;
+    RCC_OscInitStruct.LSIState = RCC_LSI_ON;
     RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
     RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
