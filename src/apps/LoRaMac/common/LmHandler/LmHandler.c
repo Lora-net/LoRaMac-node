9a6c7dc02e01282c0d6d9102196abe7925e81a49
Daniel Jaeckle <daniel.jaeckle@stackforce.de>
2021-03-17 16:07:09 +0100

    Issue #1019 - Changed the LmHandler to apply user default data rate

--- src/apps/LoRaMac/common/LmHandler/LmHandler.c
+++ src/apps/LoRaMac/common/LmHandler/LmHandler.c
@@ -263,6 +263,15 @@ LmHandlerErrorStatus_t LmHandlerInit( LmHandlerCallbacks_t *handlerCallbacks,
     }
     else
     {
+        // Configure the default datarate
+        mibReq.Type = MIB_CHANNELS_DEFAULT_DATARATE;
+        mibReq.Param.ChannelsDefaultDatarate = LmHandlerParams->TxDatarate;
+        LoRaMacMibSetRequestConfirm( &mibReq );
+
+        mibReq.Type = MIB_CHANNELS_DATARATE;
+        mibReq.Param.ChannelsDatarate = LmHandlerParams->TxDatarate;
+        LoRaMacMibSetRequestConfirm( &mibReq );
+
 #if( OVER_THE_AIR_ACTIVATION == 0 )
         // Tell the MAC layer which network server version are we connecting too.
         mibReq.Type = MIB_ABP_LORAWAN_VERSION;
