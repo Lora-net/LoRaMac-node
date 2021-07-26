d708ac5d8cda943777921ad2255c0ba0d97e8210
MedadRufus <medadrufus@gmail.com>
2021-07-27 00:24:57 +0100

    tx every 40 seconds

--- src/apps/LoRaMac/periodic-uplink-lpp/B-L072Z-LRWAN1/main.c
+++ src/apps/LoRaMac/periodic-uplink-lpp/B-L072Z-LRWAN1/main.c
@@ -59,7 +59,7 @@
 /*!
  * Defines the application data transmission duty cycle. 5s, value in [ms].
  */
-#define APP_TX_DUTYCYCLE                            5000
+#define APP_TX_DUTYCYCLE                            40000
 
 /*!
  * Defines a random delay for application data transmission duty cycle. 1s,
