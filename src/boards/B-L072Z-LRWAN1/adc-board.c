cc60506b429c39b3a46a1e5e8f6d0cb123d7f954
MedadRufus <medadrufus@gmail.com>
2021-10-26 01:47:05 +0100

    Fix adc reads (#200)
    Uses the implementation used in the old Picotracker Lora code to fix the adc read. It now works.
    
    Source adc code: https://github.com/ImperialSpaceSociety/picotracker-Lora
    
    fixes #31

--- src/boards/B-L072Z-LRWAN1/adc-board.c
+++ src/boards/B-L072Z-LRWAN1/adc-board.c
@@ -27,6 +27,8 @@
 #include "adc-board.h"
 
 ADC_HandleTypeDef AdcHandle;
+static bool AdcInitialized = false;
+
 
 void AdcMcuInit( Adc_t *obj, PinNames adcInput )
 {
@@ -45,84 +47,72 @@ void AdcMcuInit( Adc_t *obj, PinNames adcInput )
 void AdcMcuConfig( void )
 {
     // Configure ADC
-    AdcHandle.Init.OversamplingMode      = DISABLE;
-    AdcHandle.Init.ClockPrescaler        = ADC_CLOCK_SYNC_PCLK_DIV1;
-    AdcHandle.Init.Resolution            = ADC_RESOLUTION_12B;
-    AdcHandle.Init.SamplingTime          = ADC_SAMPLETIME_160CYCLES_5;
-    AdcHandle.Init.ScanConvMode          = ADC_SCAN_DIRECTION_FORWARD;
-    AdcHandle.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
-    AdcHandle.Init.ContinuousConvMode    = DISABLE;
-    AdcHandle.Init.DiscontinuousConvMode = DISABLE;
-    AdcHandle.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE;
-    AdcHandle.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_T6_TRGO;
-    AdcHandle.Init.DMAContinuousRequests = DISABLE;
-    AdcHandle.Init.EOCSelection          = ADC_EOC_SINGLE_CONV;
-    AdcHandle.Init.Overrun               = ADC_OVR_DATA_OVERWRITTEN;
-    AdcHandle.Init.LowPowerAutoWait      = DISABLE;
-    AdcHandle.Init.LowPowerFrequencyMode = DISABLE; // To be enabled only if ADC clock < 2.8 MHz
-    AdcHandle.Init.LowPowerAutoPowerOff  = DISABLE;
-    HAL_ADC_Init( &AdcHandle );
 
-    // Calibration
-    HAL_ADCEx_Calibration_Start( &AdcHandle, ADC_SINGLE_ENDED );
+    if (AdcInitialized == false)
+    {
+        AdcInitialized = true;
 
+        AdcHandle.Instance = ADC1;
+
+        AdcHandle.Init.OversamplingMode = DISABLE;
+
+        AdcHandle.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV1;
+        AdcHandle.Init.Resolution = ADC_RESOLUTION_12B;
+        AdcHandle.Init.SamplingTime = ADC_SAMPLETIME_160CYCLES_5;
+        AdcHandle.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
+        AdcHandle.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+        AdcHandle.Init.ContinuousConvMode = DISABLE;
+        AdcHandle.Init.DiscontinuousConvMode = DISABLE;
+        AdcHandle.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+        AdcHandle.Init.DMAContinuousRequests = DISABLE;
+        AdcHandle.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+        AdcHandle.Init.LowPowerAutoPowerOff = DISABLE;
+        AdcHandle.Init.LowPowerAutoWait = DISABLE;
+        AdcHandle.Init.LowPowerFrequencyMode = ENABLE;
+
+        HAL_ADC_Init(&AdcHandle);
+    }
 }
 
 uint16_t AdcMcuReadChannel( Adc_t *obj, uint32_t channel )
 {
     ADC_ChannelConfTypeDef adcConf = { 0 };
     uint16_t adcData = 0;
-    uint32_t tickStart = 0;
-    bool isAdcReady = true;
 
-    // Enable HSI
-    __HAL_RCC_HSI_ENABLE( );
-
-    // Wait till HSI is ready
-    while( __HAL_RCC_GET_FLAG( RCC_FLAG_HSIRDY ) == RESET )
+    if (AdcInitialized == true)
     {
-    }
-
-    __HAL_RCC_ADC1_CLK_ENABLE( );
-
-    adcConf.Channel = channel;
-    adcConf.Rank = ADC_RANK_CHANNEL_NUMBER;
-    HAL_ADC_ConfigChannel( &AdcHandle, &adcConf );
-
-    // Enable ADC1
-    __HAL_ADC_ENABLE( &AdcHandle );
-
-    // Wait for ADC to effectively be enabled
-    tickStart = HAL_GetTick( );
-    while( __HAL_ADC_GET_FLAG( &AdcHandle, ADC_FLAG_RDY ) == RESET )
-    {
-        if( ( HAL_GetTick( ) - tickStart ) > ADC_ENABLE_TIMEOUT )
+        /* wait the the Vrefint used by adc is set */
+        while (__HAL_PWR_GET_FLAG(PWR_FLAG_VREFINTRDY) == RESET)
         {
-            isAdcReady = false;
-            break;
-        }
-    }
+        };
 
-    if( isAdcReady != false )
-    {
-        // Start ADC Software Conversion
+        __HAL_RCC_ADC1_CLK_ENABLE();
+
+        /*calibrate ADC if any calibraiton hardware*/
+        HAL_ADCEx_Calibration_Start(&AdcHandle, ADC_SINGLE_ENDED);
+
+        /* Deselects all channels*/
+        adcConf.Channel = ADC_CHANNEL_MASK;
+        adcConf.Rank = ADC_RANK_NONE;
+        HAL_ADC_ConfigChannel( &AdcHandle, &adcConf );
+
+        /* configure adc channel */
+        adcConf.Channel = channel;
+        adcConf.Rank = ADC_RANK_CHANNEL_NUMBER;
+        HAL_ADC_ConfigChannel( &AdcHandle, &adcConf );
+
+        /* Start the conversion process */
         HAL_ADC_Start( &AdcHandle );
 
+        /* Wait for the end of conversion */
         HAL_ADC_PollForConversion( &AdcHandle, HAL_MAX_DELAY );
 
+        /* Get the converted value of regular channel */
         adcData = HAL_ADC_GetValue( &AdcHandle );
+        
+        __HAL_ADC_DISABLE( &AdcHandle );
+
+        __HAL_RCC_ADC1_CLK_DISABLE();
     }
-
-    __HAL_ADC_DISABLE( &AdcHandle );
-
-    if( ( adcConf.Channel == ADC_CHANNEL_TEMPSENSOR ) || ( adcConf.Channel == ADC_CHANNEL_VREFINT ) )
-    {
-        HAL_ADC_DeInit( &AdcHandle );
-    }
-    __HAL_RCC_ADC1_CLK_DISABLE( );
-
-    // Disable HSI
-    __HAL_RCC_HSI_DISABLE( );
-
     return adcData;
 }
