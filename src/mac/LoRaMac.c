9218d00d3d8b2cf002112cdf15019cff064b19ec
Miguel Luis <mluis@semtech.com>
2022-06-20 11:17:49 +0200

    Issue #1284 - Added missing return status initialization for Class A

--- src/mac/LoRaMac.c
+++ src/mac/LoRaMac.c
@@ -1952,6 +1952,8 @@ static LoRaMacStatus_t SwitchClass( DeviceClass_t deviceClass )
             {
                 // Revert back RxC parameters
                 Nvm.MacGroup2.MacParams.RxCChannel = Nvm.MacGroup2.MacParams.Rx2Channel;
+
+                status = LORAMAC_STATUS_OK;
             }
             if( deviceClass == CLASS_B )
             {
