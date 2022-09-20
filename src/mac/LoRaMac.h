bc2f3bc59d09f6e1ae7855c3dc45568d2b7a124a
Jan Janak <jan@janakj.org>
2022-09-20 09:29:02 +0200

    Move AdrAckLimit and AdrAckDelay to NVM MacGroup2 (#1342)
    * Teach the MIB to get/set ADR Ack limit value
    
    It would be useful for the application to have access to the ADR Ack
    limit value so that the default value obtained from regional parameters
    could be overriden.
    
    * Teach the MIB to get/set ADR Ack delay value
    
    Some applications benefit from begin able to customize the ADR Ack
    parameter default values initialized from the regional parameters.
    
    * Move AdrAckLimit and AdrAckDelay to NVM
    
    Those two parameters can be remotely updated by the network server via
    ADR and thus should probably be backed by the NVM to make sure the
    configured values survive reboots.
    
    This patch moves the two parameters to NVM.MacGroup2.MacParameters.
    
    * Teach the MIB to get/set ADR Ack default values
    
    This patch adds two new MIB types: MIB_ADR_ACK_DEFAULT_LIMIT and
    MIB_ADR_ACK_DEFAULT_DELAY. The new types can be used to configure the
    default values for these parameters, i.e., the values unaffected by ADR.
    These values are initially obtained from the regional parameters upon
    region change.
    
    * Reset AdrAck{Limit,Delay} in RsetMacParameters
    
    Since the two parameters can be set by the network server via ADR, they
    need to be reset in the function ResetMacParameters() so that there is a
    means to revert to the pre-ADR values, e.g., before OTAA Join.

--- src/mac/LoRaMac.h
+++ src/mac/LoRaMac.h
@@ -312,6 +312,16 @@ typedef struct sLoRaMacParams
      * Antenna gain of the node
      */
     float AntennaGain;
+    /*!
+     * Limit of uplinks without any donwlink response before the ADRACKReq bit
+     * will be set.
+     */
+    uint16_t AdrAckLimit;
+    /*!
+     * Limit of uplinks without any donwlink response after a the first frame
+     * with set ADRACKReq bit before the trying to regain the connectivity.
+     */
+    uint16_t AdrAckDelay;
 }LoRaMacParams_t;
 
 /*!
@@ -1437,6 +1447,10 @@ typedef struct sMlmeIndication
  * \ref MIB_REJOIN_0_CYCLE                       | YES | YES
  * \ref MIB_REJOIN_1_CYCLE                       | YES | YES
  * \ref MIB_REJOIN_2_CYCLE                       | YES | NO
+ * \ref MIB_ADR_ACK_LIMIT                        | YES | YES
+ * \ref MIB_ADR_ACK_DELAY                        | YES | YES
+ * \ref MIB_ADR_ACK_DEFAULT_LIMIT                | YES | YES
+ * \ref MIB_ADR_ACK_DEFAULT_DELAY                | YES | YES
  * \ref MIB_RSSI_FREE_THRESHOLD                  | YES | YES
  * \ref MIB_CARRIER_SENSE_TIME                   | YES | YES
  *
@@ -1871,6 +1885,22 @@ typedef enum eMib
       * LoRaWAN certification FPort handling state (ON/OFF)
       */
      MIB_IS_CERT_FPORT_ON,
+     /*!
+      * ADR ack limit value
+      */
+     MIB_ADR_ACK_LIMIT,
+     /*!
+      * ADR ack delay value
+      */
+     MIB_ADR_ACK_DELAY,
+     /*!
+      * ADR ack default limit value
+      */
+     MIB_ADR_ACK_DEFAULT_LIMIT,
+     /*!
+      * ADR ack default delay value
+      */
+     MIB_ADR_ACK_DEFAULT_DELAY,
      /*!
       * RSSI free channel threshold value (KR920 and AS923 only)
       */
@@ -2325,6 +2355,18 @@ typedef union uMibParam
      * Related MIB type: \ref MIB_IS_CERT_FPORT_ON
      */
     bool IsCertPortOn;
+    /*!
+     * ADR ack limit value
+     *
+     * Related MIB types: \ref MIB_ADR_ACK_LIMIT, MIB_ADR_ACK_DEFAULT_LIMIT
+     */
+    uint16_t AdrAckLimit;
+    /*!
+     * ADR ack delay value
+     *
+     * Related MIB types: \ref MIB_ADR_ACK_DELAY, MIB_ADR_ACK_DEFAULT_DELAY
+     */
+    uint16_t AdrAckDelay;
     /*!
      * RSSI free channel threshold (KR920 and AS923 only)
      *
